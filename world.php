b9fd84bf852d2d00d0010206c749dc35358f7c45
Delano Sewell <delanosewelledu@gmail.com>
2025-12-05 23:27:29 -0500

    Completed to generate table for Exercise 4

--- world.php
+++ world.php
@@ -1,4 +1,5 @@
 <?php
+//Connect to the database
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
@@ -7,14 +8,16 @@ $dbname = 'world';
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
+    //Get country from URL if provided
     $country = isset($_GET['country']) ? $_GET['country'] : '';
 
     if (!empty($country)) {
+        //Search for countries matching the input
         $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
         $stmt->bindValue(':country', "%$country%");
         $stmt->execute();
     } else {
-        // If no country specified, return all countries
+        //Returns all countries
         $stmt = $conn->query("SELECT * FROM countries");
     }
 
@@ -25,8 +28,29 @@ try {
     exit();
 }
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-    <li><?= htmlspecialchars($row['name']) . ' is ruled by ' . htmlspecialchars($row['head_of_state']); ?></li>
-<?php endforeach; ?>
-</ul>
+
+<?php if (count($results) > 0): ?>
+<!-- Displays the results in a table -->
+<table border="1" cellpadding="5" cellspacing="0">
+    <thead>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= htmlspecialchars($row['name']) ?></td>
+            <td><?= htmlspecialchars($row['continent']) ?></td>
+            <td><?= htmlspecialchars($row['independence_year']) ?></td>
+            <td><?= htmlspecialchars($row['head_of_state']) ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php else: ?>
+<p>No countries found.</p>
+<?php endif; ?>
